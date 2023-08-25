fc877258b5d78102522860b0913bb0351d77191d
ZH-Matthew <m.zhitenev23@gmail.com>
2023-08-25 03:12:41 +0300

    Сделал все пункты согласно ДЗ

--- src/main/java/ru/hogwarts/school/controller/FacultyController.java
+++ src/main/java/ru/hogwarts/school/controller/FacultyController.java
@@ -1,4 +1,56 @@
 package ru.hogwarts.school.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.service.FacultyService;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("faculty")
 public class FacultyController {
+    private FacultyService facultyService;
+
+    public FacultyController(FacultyService facultyService) {
+        this.facultyService = facultyService;
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Faculty> getFaculty(@PathVariable long id){
+        Faculty faculty = facultyService.findFaculty(id);
+        if(faculty==null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(faculty);
+    }
+
+    @GetMapping("byColor/{id}")
+    public ResponseEntity<Collection<Faculty>> getFacultyByColor(@PathVariable String id) {
+        Collection<Faculty> facultyByColor = facultyService.getFacultyByColor(id);
+        if (facultyByColor.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(facultyByColor);
+    }
+
+    @PostMapping
+    public Faculty postFaculty(@RequestBody Faculty faculty){
+        return facultyService.createFaculty(faculty);
+    }
+
+    @PutMapping
+    public ResponseEntity<Faculty> putFaculty(@RequestBody Faculty faculty){
+        Faculty foundFaculty = facultyService.editFaculty(faculty);
+        if (foundFaculty == null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+        return ResponseEntity.ok(foundFaculty);
+    }
+
+    @DeleteMapping("{id}")
+    public Faculty deleteFaculty(@PathVariable long id){
+        return facultyService.deleteFaculty(id);
+    }
 }
--- src/main/java/ru/hogwarts/school/service/FacultyService.java
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -2,19 +2,42 @@ package ru.hogwarts.school.service;
 
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.model.Faculty;
-import ru.hogwarts.school.model.Student;
+import java.util.*;
 
-import java.util.HashMap;
-import java.util.Map;
 @Service
 public class FacultyService {
-    private final int count = 0;
-    private final Map<Long, Student> facultyMap = new HashMap<>();
+    private long count = 0;
+    private final Map<Long, Faculty> facultyMap= new HashMap<>();
 
     public Faculty createFaculty(Faculty faculty){
+        faculty.setId(++count);
+        facultyMap.put(count,faculty);
+        return faculty;
+    }
+
+    public Faculty findFaculty(long id){
+        return facultyMap.get(id);
+    }
+
+    public Faculty editFaculty(Faculty faculty){
+        if(facultyMap.containsKey(faculty.getId())) {
+            facultyMap.put(faculty.getId(), faculty);
+            return faculty;
+        }
         return null;
     }
 
+    public Faculty deleteFaculty(long id){
+        return facultyMap.remove(id);
+    }
 
-
+    public Collection<Faculty> getFacultyByColor(String id) {
+        List<Faculty> listFacultyByColor = new ArrayList<>();
+        for (Map.Entry<Long, Faculty> entry: facultyMap.entrySet()) {
+            if(entry.getValue().getColor().equals(id)){
+                listFacultyByColor.add(entry.getValue());
+            }
+        }
+        return listFacultyByColor;
+    }
 }
