5508697f85c906aa12c69faa99baf883828e12ea
ZH-Matthew <m.zhitenev23@gmail.com>
2023-09-22 16:58:54 +0300

    HW_4_2_SQL Создал 3 файла, внутри них SQL запросы по ТЗ

---
+++ scripts421.sql
@@ -0,0 +1,9 @@
+ALTER TABLE student ADD CONSTRAINT age_constraint CHECK (age > 15);
+
+ALTER TABLE student ADD CONSTRAINT nickname_unique UNIQUE (name);
+
+ALTER  TABLE student ALTER COLUMN name SET NOT NULL;
+
+ALTER TABLE faculty ADD CONSTRAINT name_color_unique UNIQUE (name, color);
+
+ALTER TABLE student ALTER COLUMN age SET DEFAULT 20;
